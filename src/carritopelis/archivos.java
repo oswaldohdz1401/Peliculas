d7d62aa40f2132f9fed1efa3dc68d9bf0dd1416b
oswaldohernandez11 <oswaldo_hernandezj@hotmail.com>
2017-12-09 13:34:34 -0600

    Estandar de calidad Java

--- src/carritopelis/archivos.java
+++ src/carritopelis/archivos.java
@@ -1,6 +1,4 @@
-
 package carritopelis;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -12,21 +10,23 @@ import javax.swing.JTable;
  */
 public class archivos {
     
-    public void guardaTabla(JTable catalogo,String tiendaPeliculas){
+    public void guardaTabla (JTable catalogo,String tiendaPeliculas) {
         int i;
         int j;
         try {
-            
-            File fichero = new File(tiendaPeliculas);
-            if(fichero.exists()){
+            File fichero = new File (tiendaPeliculas);
+            if (fichero.exists()) {
                 fichero.delete();
             }
-            
-            BufferedWriter bfw = new BufferedWriter(new FileWriter(tiendaPeliculas));
-            for (i = 0 ; i < catalogo.getRowCount(); i++){
-                for(j = 0 ; j < catalogo.getColumnCount();j++){
+            BufferedWriter bfw;
+            bfw = new BufferedWriter (new FileWriter (tiendaPeliculas) );
+            for (i = 0 ; i < catalogo.getRowCount(); i++) {
+                for(j = 0 ; j < catalogo.getColumnCount();j++) {
                     bfw.write(catalogo.getValueAt(i,j).toString());
-                    if (j < catalogo.getColumnCount() -1) { //agrega separador "," si no es el ultimo elemento de la fila.
+                    if (j < catalogo.getColumnCount() -1) { 
+                        /*agrega separador "," 
+                         *si no es el ultimo elemento de la fila.
+                        */
                         bfw.write(",");
                     }
                 }
@@ -35,7 +35,9 @@ public class archivos {
             bfw.close(); //cierra archivo!
             System.out.println("El archivo fue salvado correctamente!");
         } catch (IOException e) {
-            System.out.println("ERROR: Ocurrio un problema al salvar el archivo!" + e.getMessage());
+            System.out.println("ERROR: "
+                               +"Ocurrio un problema al salvar el archivo!"
+                               +e.getMessage());
         }
     }
     
